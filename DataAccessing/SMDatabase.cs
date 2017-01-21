ba55e5ec1a323c280bc3845825d99027a84441a7
longthan01 <longthan385922@gmail.com>
2017-01-22 00:32:18 +0700

    Initial

---
+++ DataAccessing/SMDatabase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data.Sql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessing
+{
+    public class SMDatabase : SqlDatabase
+    {
+        public SMDatabase(string connString) : base(connString)
+        {
+        } 
+        public DataTable ExecuteDataTable(string procName, SMParameterCollection parameters)
+        {
+            DbCommand cmd = this.GetStoredProcCommand(procName);
+            foreach (var param in parameters)
+            {
+                cmd.Parameters[param.Name].Value = param.Value;
+                cmd.Parameters[param.Name].DbType = param.DbType;
+            }
+            var dataSet = this.ExecuteDataSet(cmd);
+            DataTable dt = dataSet.Tables.Count == 0 ? new DataTable() : dataSet.Tables[0];
+            return dt;
+        }
+    }
+}
---
+++ DataAccessing/SMParameter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessing
+{
+    public class SMParameter
+    {
+        public string Name { get; set; }
+        public object Value { get; set; }
+        public DbType DbType { get; set; } 
+    }
+    public class SMParameterCollection : List<SMParameter>
+    {
+        public SMParameterCollection AddParam(SMParameter item)
+        {
+            if (item != null)
+            {
+                this.Add(item);
+            }
+            return this;
+        }
+    }
+}
